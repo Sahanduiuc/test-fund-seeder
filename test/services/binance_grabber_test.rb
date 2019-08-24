9c6ed420947da944d4b1eea3fd81ea09d8272a36
Vadim Glebov <vglebov@gmail.com>
2019-08-24 23:00:59 +0600

    fixed test binance_grabber_test.rb

--- test/services/binance_grabber_test.rb
+++ test/services/binance_grabber_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 ANOTHER_UPDATE_TIME = "9999999999999"
 
+def change_update_time
+  #FIXME replace change time in DB on change time in stub service response
+  r = RequestResult.all.last
+  r.raw_data["updateTime"] = ANOTHER_UPDATE_TIME
+  r.save!
+end
+
 class BinanceGrabberTest < ActiveSupport::TestCase
+
   test "Grabber should save new record if 'updateTime' of account was changed" do
     grabber = BinanceGrabber.new
     grabber.fetch_account
@@ -11,12 +19,6 @@ class BinanceGrabberTest < ActiveSupport::TestCase
     assert_equal 2, RequestResult.all.count
   end
 
-  def self.change_update_time
-    #FIXME replace change time in DB on change time in stub service response
-    r = RequestResult.all.last
-    r.raw_data["updateTime"] = ANOTHER_UPDATE_TIME
-    r.save!
-  end
 
   test "Grabber should'nt save new record if 'updateTime' of account was not changed" do
     grabber = BinanceGrabber.new
