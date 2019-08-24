4288bc5041bfd774f2ce540c367bd9951d5727b3
Vadim Glebov <vglebov@gmail.com>
2019-08-24 22:59:12 +0600

    implemented requirements for parse method for BinanceParser service class "Parser should parse only non-parser RequestResult records" and "It should save balances array only but without zero values."

--- app/services/binance_parser.rb
+++ app/services/binance_parser.rb
@@ -1,2 +1,10 @@
 class BinanceParser
+  def parse
+    RequestResult.where(parsed_data: nil).find_each do |rr|
+      rr.parsed_data = rr.raw_data["balances"].select do |balance|
+        BigDecimal(balance["free"]) + BigDecimal(balance["locked"]) > 0
+      end
+      rr.save!
+    end
+  end
 end
---
+++ test/services/binance_parser_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+BALANCES = [
+  {
+    "asset" => 'BTC',
+    "free" => '4723846.89208129',
+    "locked" => '0.00000000'
+  },
+  {
+    "asset" => 'LTC',
+    "free" => '4763368.68006011',
+    "locked" => '0.00000000'
+  },
+  {
+    "asset" => 'XXX',
+    "free" => '0.00000000',
+    "locked" => '0.00000000'
+  }
+].freeze
+
+class BinanceParserTest < ActiveSupport::TestCase
+  # - [ ] Parser should parse only non-parser RequestResult records.
+  test 'It should save balances array only but without zero values' do
+    parser = BinanceParser.new
+    rr1 = RequestResult.create(raw_data: {'balances' => BALANCES})
+    rr2 = RequestResult.create(raw_data: {'balances' => BALANCES})
+    parser.parse
+    rr1.reload
+    rr2.reload
+    assert_equal BALANCES[0..1], rr1.parsed_data, "parsed_data can't contains zero values"
+    assert_equal BALANCES[0..1], rr2.parsed_data, "parsed_data can't contains zero values"
+  end
+
+  test 'Parser should parse only non-parser RequestResult records' do
+    parser = BinanceParser.new
+    rr = RequestResult.create(raw_data: {'balances' => BALANCES}, parsed_data: BALANCES)
+    parser.parse
+    rr.reload
+    assert_equal BALANCES, rr.parsed_data, "parsed_data should not be updated"
+  end
+end
