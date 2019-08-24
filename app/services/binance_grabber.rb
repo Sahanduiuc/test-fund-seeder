0c5af169287a186d31e8e51014e4800ab5702bf3
Vadim Glebov <vglebov@gmail.com>
2019-08-24 23:54:04 +0600

    added lock a last record for consistency purposes in BinanceGrabber fetch_account, cleaned code.

--- app/services/binance_grabber.rb
+++ app/services/binance_grabber.rb
@@ -2,11 +2,18 @@
 
 class BinanceGrabber
   def fetch_account
-    client = Binance::Client::REST.new api_key: BINANCE_API_KEY, secret_key: BINANCE_SECRET_KEY
-    account_info = client.account_info
-    result = RequestResult.order(:id).last
-    if result.blank? || account_info['updateTime'] != result.raw_data['updateTime']
-      RequestResult.create!(raw_data: account_info)
+    client = Binance::Client::REST.new(
+      api_key: BINANCE_API_KEY,
+      secret_key: BINANCE_SECRET_KEY
+    )
+    account = client.account_info
+    RequestResult.transaction do
+      # Lock the last record for consistency
+      # purposes when more than one grabber runs
+      record = RequestResult.lock.last
+      if record.blank? || record.raw_data['updateTime'] != account['updateTime']
+        RequestResult.create!(raw_data: account)
+      end
     end
   end
 end
