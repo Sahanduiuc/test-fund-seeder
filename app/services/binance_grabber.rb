7fcb0598282b6ede4a50a372b2735da114aa7b88
Vadim Glebov <vglebov@gmail.com>
2019-08-27 21:56:24 +0600

    refactored binance_grabber.rb, extracted method client

--- app/services/binance_grabber.rb
+++ app/services/binance_grabber.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 class BinanceGrabber
+  attr_writer :client
+
   def fetch_account
-    client = Binance::Client::REST.new(
-      api_key: BINANCE_API_KEY,
-      secret_key: BINANCE_SECRET_KEY
-    )
     account = client.account_info
     RequestResult.transaction do
-      # Lock the last record for consistency purposes when more than one grabber runs
       record = RequestResult.lock.last
       if record.blank? || record.raw_data['updateTime'] != account['updateTime']
         RequestResult.create!(raw_data: account)
       end
     end
+  end
+
+  private
+
+  def client
+    @client ||= Binance::Client::REST.new(
+      api_key: BINANCE_API_KEY,
+      secret_key: BINANCE_SECRET_KEY
+    )
   end
 end
