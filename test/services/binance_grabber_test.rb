b6fbc51706b1e07af7fa651ddac0fd1172571131
Vadim Glebov <vglebov@gmail.com>
2019-08-27 22:39:14 +0600

    rewrited binance_grabber_test.rb with stub client

--- test/services/binance_grabber_test.rb
+++ test/services/binance_grabber_test.rb
@@ -2,26 +2,54 @@
 
 require 'test_helper'
 
-ANOTHER_UPDATE_TIME = '9999999999999'
+ANOTHER_TIME = 9_999_999_999_999
 
-def change_update_time
-  # FIXME: replace change time in DB on change time in stub service response
-  r = RequestResult.all.last
-  r.raw_data['updateTime'] = ANOTHER_UPDATE_TIME
-  r.save!
+ACCOUNT_INFO = {
+  'makerCommission' => 10,
+  'takerCommission' => 10,
+  'buyerCommission' => 0,
+  'sellerCommission' => 0,
+  'canTrade' => true,
+  'canWithdraw' => true,
+  'canDeposit' => true,
+  'updateTime' => 1_545_034_863_172,
+  'accountType' => 'MARGIN',
+  'balances' => [
+    { 'asset' => 'BTC', 'free' => '0.00000000', 'locked' => '0.00000000' },
+    { 'asset' => 'ETH', 'free' => '0.02320440', 'locked' => '0.00000000' },
+    { 'asset' => 'BNB', 'free' => '0.39970000', 'locked' => '0.00000000' },
+    { 'asset' => 'TOMO', 'free' => '0.00000000', 'locked' => '0.00000000' }
+  ]
+}.freeze
+
+class StubClient
+  attr_reader :account_info
+
+  def initialize
+    @account_info = ACCOUNT_INFO.dup
+  end
+
+  def update_time=(update_time)
+    @account_info['updateTime'] = update_time
+  end
 end
 
 class BinanceGrabberTest < ActiveSupport::TestCase
+  attr_accessor :grabber, :client
+  setup do
+    @grabber = BinanceGrabber.new
+    @client = StubClient.new
+    @grabber.client = @client
+  end
+
   test "Grabber should save new record if 'updateTime' of account was changed" do
-    grabber = BinanceGrabber.new
     grabber.fetch_account
-    change_update_time
+    client.update_time = ANOTHER_TIME
     grabber.fetch_account
     assert_equal 2, RequestResult.all.count
   end
 
   test "Grabber should'nt save new record if 'updateTime' of account was not changed" do
-    grabber = BinanceGrabber.new
     grabber.fetch_account
     grabber.fetch_account
     assert_equal 1, RequestResult.all.count
