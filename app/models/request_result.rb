0bd0885b7a2660612180bbeaf12da4a0008016aa
Vadim Glebov <vglebov@gmail.com>
2019-08-31 21:27:22 +0600

    applied rubocop recommendations to request_result.rb

--- app/models/request_result.rb
+++ app/models/request_result.rb
@@ -7,31 +7,31 @@ class RequestResult < ApplicationRecord
   validate :parsed_data_must_be_an_array
 
   def raw_data_must_be_a_hash
-    unless raw_data.is_a?(Hash)
-      errors.add(
-        :raw_data,
-        "Must be a Hash but it is a #{raw_data.class.name}"
-      )
-    end
+    return if raw_data.is_a?(Hash)
+
+    errors.add(
+      :raw_data,
+      "Must be a Hash but it is a #{raw_data.class.name}"
+    )
   end
 
   def raw_data_should_contain_balances
     balances = raw_data.dig('balances')
-    if !balances || !balances.is_a?(Array)
-      errors.add(
-        :raw_data,
-        'Should contains balances array'
-      )
-    end
+    return if balances&.is_a?(Array)
+
+    errors.add(
+      :raw_data,
+      'Should contains balances array'
+    )
   end
 
   def parsed_data_must_be_an_array
-    if parsed_data && !parsed_data.is_a?(Array)
-      errors.add(
-        :parsed_data,
-        "Must be an Array but it is a #{parsed_data.class.name}"
-      )
-    end
+    return if !parsed_data || parsed_data.is_a?(Array)
+
+    errors.add(
+      :parsed_data,
+      "Must be an Array but it is a #{parsed_data.class.name}"
+    )
   end
 
   serialize :raw_data
