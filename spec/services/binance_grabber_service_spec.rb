2de90ba17ca7a66a7bd95272cdfef9ca188847dc
Vadim Glebov <vglebov@gmail.com>
2019-08-31 19:48:48 +0600

    added spec binance_grabber_service_spec.rb

---
+++ spec/services/binance_grabber_service_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BinanceGrabberService, type: :service do
+  it "Grabber should save new record if 'updateTime' of account was changed" do
+    count_before = RequestResult.all.count
+    VCR.use_cassette(
+      "Binance client.account_info 2 responses with different updateTime",
+      :match_requests_on => [],
+      :record => :once,
+      allow_playback_repeats: true
+    ) do
+      BinanceGrabberService.call # updateTime: 1111111111111
+      BinanceGrabberService.call # updateTime: 2222222222222
+      BinanceGrabberService.call # updateTime: 2222222222222
+      BinanceGrabberService.call # updateTime: 2222222222222
+      BinanceGrabberService.call # updateTime: 2222222222222
+    end
+    pp RequestResult.all
+    expect(RequestResult.all.count).to eq(count_before + 2)
+  end
+end
